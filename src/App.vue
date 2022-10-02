8a8ef0afebfbc07622cd74cc8d2cd0be155fed2e
weihung-1010 <eagle20150813@gmail.com>
2022-10-03 04:04:10 +0800

    Twitter Project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="simple-twitter">
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/components/icon.vue
@@ -0,0 +1,24 @@
+
+
+<script>
+export default {
+  data() {
+    return {
+      logo: "https://i.postimg.cc/Dfp23k8g/logo-2x.png",
+      Home: "https://i.postimg.cc/MK4VTFr0/home.png",
+      HomeActive: "https://i.postimg.cc/V6WM7sgm/Home-Active.png",
+      Setting: "https://i.postimg.cc/qqZNYN3V/Setting.png",
+      SettingActive: "https://i.postimg.cc/FzfrYD78/Setting-Active.png",
+      User: "https://i.postimg.cc/L8VJKQf3/User.png",
+      UserActive: "https://i.postimg.cc/j2VxJyPj/User-Active.png",
+      Bell: "https://i.postimg.cc/8zk1WpJL/bell.png",
+      BellActive: "https://i.postimg.cc/5N7r7bKB/bell-active.png",
+      SignOut: "https://i.postimg.cc/fyH8rfCz/signout.png",
+      iconX: "https://i.postimg.cc/G3MzrzPr/iconX.png",
+      iconLiked: "https://i.postimg.cc/rFB1FHD6/icon-Liked.png",
+      iconLike: "https://i.postimg.cc/43TQw0Gv/iconLike.png",
+      camera: "https://i.postimg.cc/Y9CgsKsF/camera.png",
+    };
+  },
+};
+</script>
