95ee70f9f8475a4c7a3d9f4ba2f03a970907fe3e
weihung-1010 <eagle20150813@gmail.com>
2022-10-05 02:00:51 +0800

    add Login & Admin & SignUp

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import Login from '../views/Login.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'sign-in',
+    component:() => import ('../views/SignUp.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('../views/Admin.vue')
+  },
   {
     path: '*',
     name: 'not-found',
